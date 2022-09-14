24f97f7b513fb260342fbaed5448dc0ca5248cea
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-09-14 16:29:40 -0700

    Check in image and dockerfile

--- template/{{ cookiecutter.project_slug }}/{{ cookiecutter.package }}/_image.py
+++ template/{{ cookiecutter.project_slug }}/{{ cookiecutter.package }}/_image.py
@@ -1,11 +1,14 @@
 from {{ cookiecutter.package }} import __version__
 
+
 {%- if cookiecutter.container_registry == "DockerHub" -%}
 
+# Stanford Container Registry image
 IMAGE = f"scr.svc.stanford.edu/{{ cookiecutter.registry_username }}/{{ cookiecutter.project_slug }}:{__version__}"
 
 {%- elif cookiecutter.container_registry == "Stanford Container Registry" -%}
 
+# DOCKERHUB IMAGE
 IMAGE = f"registry.hub.docker.com/{{ cookiecutter.registry_username }}/{{ cookiecutter.project_slug }}:{__version__}"
 
 {% endif %}
