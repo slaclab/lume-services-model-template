6f1a0c878099eeaf91efae22262b498d83a018da
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-09-14 14:35:45 -0700

    Remove comments from image

--- template/{{ cookiecutter.project_slug }}/{{ cookiecutter.package }}/_image.py
+++ template/{{ cookiecutter.project_slug }}/{{ cookiecutter.package }}/_image.py
@@ -1,12 +1,10 @@
 from {{ cookiecutter.package }} import __version__
 
 {%- if cookiecutter.container_registry == "DockerHub" -%}
-# Possible license content here
 
 IMAGE = f"scr.svc.stanford.edu/{{ cookiecutter.registry_username }}/{{ cookiecutter.project_slug }}:{__version__}"
 
 {%- elif cookiecutter.container_registry == "Stanford Container Registry" -%}
-# More possible license content here
 
 IMAGE = f"registry.hub.docker.com/{{ cookiecutter.registry_username }}/{{ cookiecutter.project_slug }}:{__version__}"
 
