52cc238db8b3c774137d50601f9400bbb5508057
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-09-15 13:51:56 -0700

    Check in image with corrected path

--- template/{{ cookiecutter.project_slug }}/{{ cookiecutter.package }}/_image.py
+++ template/{{ cookiecutter.project_slug }}/{{ cookiecutter.package }}/_image.py
@@ -3,12 +3,12 @@ from {{ cookiecutter.package }} import __version__
 
 {%- if cookiecutter.container_registry == "DockerHub" -%}
 
-# Stanford Container Registry image
-IMAGE = f"scr.svc.stanford.edu/{{ cookiecutter.registry_username }}/{{ cookiecutter.project_slug }}:{__version__}"
+# DOCKERHUB IMAGE
+IMAGE = f"registry.hub.docker.com/{{ cookiecutter.registry_username }}/{{ cookiecutter.project_slug }}:{__version__}"
 
 {%- elif cookiecutter.container_registry == "Stanford Container Registry" -%}
 
-# DOCKERHUB IMAGE
-IMAGE = f"registry.hub.docker.com/{{ cookiecutter.registry_username }}/{{ cookiecutter.project_slug }}:{__version__}"
+# Stanford Container Registry image
+IMAGE = f"scr.svc.stanford.edu/{{ cookiecutter.registry_username }}/{{ cookiecutter.project_slug }}:{__version__}"
 
 {% endif %}
