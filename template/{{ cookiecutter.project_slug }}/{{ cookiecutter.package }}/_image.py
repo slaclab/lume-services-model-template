3fed667fd3ca7d5da0b63c390efd8256f7c36979
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-09-16 12:28:29 -0700

    Fix image

--- template/{{ cookiecutter.project_slug }}/{{ cookiecutter.package }}/_image.py
+++ template/{{ cookiecutter.project_slug }}/{{ cookiecutter.package }}/_image.py
@@ -4,11 +4,11 @@ from {{ cookiecutter.package }} import __version__
 {%- if cookiecutter.container_registry == "DockerHub" -%}
 
 # DOCKERHUB IMAGE
-IMAGE = f"registry.hub.docker.com/{{ cookiecutter.registry_username }}/{{ cookiecutter.project_slug }}:{__version__}"
+IMAGE = f"registry.hub.docker.com/{{ cookiecutter.registry_username }}/{{ cookiecutter.project_slug }}:v{__version__}"
 
 {%- elif cookiecutter.container_registry == "Stanford Container Registry" -%}
 
 # Stanford Container Registry image
-IMAGE = f"scr.svc.stanford.edu/{{ cookiecutter.registry_username }}/{{ cookiecutter.project_slug }}:{__version__}"
+IMAGE = f"scr.svc.stanford.edu/{{ cookiecutter.registry_username }}/{{ cookiecutter.project_slug }}:v{__version__}"
 
 {% endif %}
