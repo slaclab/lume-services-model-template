840c59010c7a0b17a8c17920e4b7cd0e9a7bf7ed
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-10-08 14:41:07 -0700

    Fix @formatting

--- template/{{ cookiecutter.repo_name }}/{{ cookiecutter.package }}/_image.py
+++ template/{{ cookiecutter.repo_name }}/{{ cookiecutter.package }}/_image.py
@@ -1,6 +1,5 @@
 from {{ cookiecutter.package }} import __version__
 
-
 {%- if cookiecutter.container_registry == "DockerHub" -%}
 
 # DOCKERHUB IMAGE
@@ -9,6 +8,6 @@ IMAGE = f"registry.hub.docker.com/{{ cookiecutter.registry_username }}/{{ cookie
 {%- elif cookiecutter.container_registry == "Stanford Container Registry" -%}
 
 # Stanford Container Registry image
-IMAGE = f"scr.svc.stanford.edu/{{ cookiecutter.registry_username.replace('@', '') }}/{{ cookiecutter.repo_name }}:v{__version__}"
+IMAGE = f"scr.svc.stanford.edu/{{ cookiecutter.registry_username }}/{{ cookiecutter.repo_name }}:v{__version__}"
 
 {% endif %}
